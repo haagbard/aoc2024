269bf826ef8e6f44088c32407e199b9f3744145b
Richard Haaga <richard.haaga@avanza.se>
2024-12-05 14:15:07 +0100

    Day 5

---
+++ day5/day5_1.py
@@ -0,0 +1,43 @@
+import re
+
+file = 'aoc2024/day5/input.txt'
+
+with open(file, 'r') as file:
+    lines = file.read().splitlines()
+
+updates_re = r"(\d+)\|(\d+)"
+
+updates = []
+prints_lines = []
+
+for line in lines:
+    if re.match(updates_re, line):
+        s = re.search(updates_re, line)
+        updates.append((s.group(1), s.group(2)))
+    elif line.rstrip() != '':
+        prints_lines.append(line)
+
+def find_middle_value(prints):
+    middleIndex = int((len(prints) - 1)/2)
+    return int(prints[middleIndex])
+
+def is_valid(prints):
+    seen = set()
+    for value in prints:
+        seen.add(value)
+
+        for a, b in updates:
+            if a in seen and b in seen:
+                if prints.index(b) < prints.index(a):
+                    return False
+    return True
+
+sum = 0
+
+for print_line in prints_lines:
+    prints = print_line.split(',')
+    success = is_valid(prints)
+    if success:
+        sum += find_middle_value(prints)
+
+print(sum)
